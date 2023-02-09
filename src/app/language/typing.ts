7853af709068f3610969ac208b5c2069dd63e8bd
ClementSyu <clement.syu.csf@gmail.com>
2023-02-09 13:19:51 +0800

    type - WeatherRawDataLocation

--- src/app/language/typing.ts
+++ src/app/language/typing.ts
@@ -1,2 +1,3 @@
 import { CustomTypeOptions } from "react-i18next";
+export type LanguageNamespace = keyof CustomTypeOptions["resources"];
 export type LanguageResourceContentKey<T extends keyof CustomTypeOptions["resources"]> = keyof CustomTypeOptions["resources"][T];
