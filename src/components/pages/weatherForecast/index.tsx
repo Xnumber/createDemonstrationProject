93162c8cbc48d79a5eb1947dbebb96f53cdb8c71
ClementSyu <clement.syu.csf@gmail.com>
2023-02-11 12:39:33 +0800

    compare state manage

--- src/components/pages/weatherForecast/index.tsx
+++ src/components/pages/weatherForecast/index.tsx
@@ -1,11 +1,17 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useCallback } from "react";
 import { MultipleSelect } from "molecules/multipleSelect";
 import { useTranslation } from "react-i18next";
-import { Typography, Checkbox, FormControlLabel } from "@mui/material";
+import { Typography } from "@mui/material";
 import  Grid2  from "@mui/material/Unstable_Grid2";
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
 import { ChartHtmlLegend } from "molecules/chart/lengend";
 import { Chart } from "react-chartjs-2";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import { Compare } from "./typing";
 
 ChartJS.register(
 	CategoryScale,
@@ -50,29 +56,36 @@ const data = {
 
 function WeatherForecast() {
 	const { t } = useTranslation("weather-forecast");
+	const [compare, setCompare] = useState<Compare>();
 	const chartRef = useRef<ChartJS>(null);
-	
 	const chartTitle = "Title";
+	const handleSetCompare: React.ReactEventHandler<HTMLInputElement> = useCallback((e) => {
+		setCompare(e.currentTarget.value as Compare);
+	}, []);
 
 	return <>
 		<Typography variant="h1">
 			{t("weather-forecast")}
 		</Typography>
 		<Grid2 container columns={12}>
-			<Grid2 md={3} xs={12}>
-				<FormControlLabel
-					label={t("by-location")}
-					control={ <Checkbox />}
-				/>
-			</Grid2>
-			<Grid2 md={3} xs={12}>
-				<FormControlLabel
-					label={t("by-element")}
-					control={ <Checkbox />}
-				/>
+			<Grid2 xs={12}>
+				<FormControl>
+					<FormLabel id="compare">
+						{t("compare")}
+					</FormLabel>
+					<RadioGroup
+						row
+						aria-labelledby="compare"
+						name="compare-group"
+						value={compare}
+						onChange={handleSetCompare}
+					>
+						<FormControlLabel value="location" control={<Radio />} label={t("location")} />
+						<FormControlLabel value="element" control={<Radio />} label={t("element")} />
+					</RadioGroup>
+				</FormControl>
 			</Grid2>
 		</Grid2>
-
 		<Grid2 container columns={12}>
 			<Grid2 xs={12} md={3}>
 				<MultipleSelect 
@@ -80,7 +93,6 @@ function WeatherForecast() {
 					options={[]}
 					callback={(e) => {console.log(e);}}
 				/>
-
 			</Grid2>
 			<Grid2 xs={12} md={3}>
 				<MultipleSelect 
