8cf016efc639a38b478918ae6b8cbe20872047bc
ClementSyu <clement.syu.csf@gmail.com>
2023-03-11 13:41:07 +0800

    feat: adjust language
    topic
    - new translations
    - applying home.json to typing
    
    modified
    - src/app/language/locales/en/simple-content-management.json
    - src/app/language/locales/en/water.json
    - src/app/language/locales/zh/simple-content-management.json
    
      new translations
    
    - src/app/language/react-i18next.d.ts
    
      applying home.json to typing

--- src/app/language/react-i18next.d.ts
+++ src/app/language/react-i18next.d.ts
@@ -1,12 +1,13 @@
 import translationEn from "./locales/en/translation.json";
 import WeatherForecast from "./locales/en/weather-forecast.json";
 import Water from "./locales/en/water.json";
-
+import HomeEn from "./locales/en/home.json";
 import SimpleContentManagementEn from "./locales/en/simple-content-management.json";
 declare module "react-i18next" {
 	interface CustomTypeOptions {
 		defaultNS: typeof translationEn;
 		resources: {
+			"home": typeof HomeEn;
 			"translation": typeof translationEn;
 			"weather-forecast": typeof WeatherForecast;
 			"water": typeof Water;
