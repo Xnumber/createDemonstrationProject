51d348175d6e3717fd0f0c4c2514eccb1b3636b1
ClementSyu <clement.syu.csf@gmail.com>
2023-03-13 18:11:35 +0800

    chore: api base url for dev and prod enviroment
    topic
    - apply api base url for dev and prod using DefinePlugin
    
    created
     - src\type.d.ts
    
      typing for the global variable
    
    modified
    - config\webpack.prod.js
    - config\webpack.dev.js
    
      apply DefinePlugin
    
    - src\app\service\simpleContentManagement\api.ts
    
      apply baseUrl with the variable
      delete unused line

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const { DefinePlugin } = require("webpack");
 module.exports = merge(common("production"), {
 	mode: "production",
 	output: {
@@ -33,4 +34,9 @@ module.exports = merge(common("production"), {
 		// 'react-router-dom': ["createBrowserRouter", "react-router-dom"]
 	},
 	// https://unpkg.com/react@18.2.0/umd/react.production.min.js
+	plugins: [
+		new DefinePlugin({
+			SIMPLE_CONTENT_MANAGEMENT_API_BASE_URL: JSON.stringify("http://localhost/api")
+		})
+	]
 });
---
+++ src/type.d.ts
@@ -0,0 +1,1 @@
+declare const SIMPLE_CONTENT_MANAGEMENT_API_BASE_URL: string;
