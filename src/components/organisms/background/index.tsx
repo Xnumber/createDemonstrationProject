34fc2ddd39504c71b24e27bba6e60cbb2dd30002
ClementSyu <clement.syu.csf@gmail.com>
2023-03-20 17:06:08 +0800

    feat: background component
    topic
    - create a component controls bkg according to page route changing
    
    created
    - src\components\organisms\background\const.ts
    - src\components\organisms\background\index.tsx
    - src\components\organisms\background\style.scss
    
      image url, user interface, style
    
    modified
    - src\components\pages\features\index.tsx
    - src\components\pages\planning\index.tsx
    
     owing to the component, bkg related functions could be deleted
    
    - src\components\templates\root\index.tsx
    
      apply the component

---
+++ src/components/organisms/background/index.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./style.scss";
+import { useLocation } from "react-router-dom";
+import { routes } from "src/app/router/routes";
+import { backgrounds, fadeOutDuration, fadeInDuration } from "./const";
+import { disableLoading, loading } from "src/lib/loading";
+import { useAppSelector } from "src/app/hooks";
+const Background = () => {
+	const bkgRef =  useRef<HTMLDivElement>(null);
+	const bkg =  useRef(new Image());
+	const location = useLocation();
+	const currentPageName = routes.find(r => r.path?.replace("/:lng", "") === location.pathname.replace(/\/zh|\/en/, ""));
+	const [bkgState, setBkgState] = useState<"fadeIn" | "fadeOut" | "initial" | "complete" >("initial"); 
+	const [transition, setTransition] = useState<string>("2s");
+	const loadingQueue = useAppSelector(state => state.loading.loadingQueue);
+
+	const loadImage = async () => {
+		const bkgBlock = bkgRef.current;
+		loading("LoadBackground");
+		await new Promise<void>((res) => {
+			if (bkgBlock && currentPageName && backgrounds[currentPageName.name]) {
+				const bkgUrl = backgrounds[currentPageName.name];
+				bkg.current.src = bkgUrl;
+				bkg.current.onload = () => {
+					disableLoading("LoadBackground");
+					setTimeout(() => {
+						bkgBlock.style.backgroundImage = `url(${bkgUrl})`;
+						res();
+					}, fadeOutDuration);
+				};
+			} else if(bkgBlock){
+				disableLoading("LoadBackground");
+				setTimeout(() => {
+					bkgBlock.style.backgroundImage = "";
+					res();
+				}, fadeOutDuration);
+			}
+		});
+	};
+
+	const bkgControl = async () => {
+		await loadImage();
+		setTransition(fadeInDuration + "ms");
+		setBkgState("complete");
+	};
+
+	useEffect(() => {
+		setTransition(fadeOutDuration + "ms");
+		setBkgState("initial");
+		bkgControl();
+	}, [location]);
+
+	useEffect(() => {
+		if (loadingQueue.find(l => l.event === "Change Route")) {
+			setTransition(fadeOutDuration + "ms");
+			setBkgState("initial");
+		}
+	}, [loadingQueue]);
+
+	return <div
+		style={{
+			transition: transition
+		}}
+		ref={bkgRef}
+		className={`m-background m-background--${bkgState}`}
+	>
+	</div>;
+};
+
+export { Background };
--- src/components/pages/features/index.tsx
+++ src/components/pages/features/index.tsx
@@ -1,36 +1,15 @@
 import { CollapseMenu } from "organisms/collapseMenu";
 import { Typography, Box } from "@mui/material";
-import React, { useEffect, useRef } from "react";
-import { bkgUrl, menu } from "./const";
+import React from "react";
+import { menu } from "./const";
 import { useTranslation } from "react-i18next";
 import { XCenter } from "templates/xCenter";
-import "./style.scss";
+
 const Features = () => {
 	const { t } = useTranslation("home");
-	const bkgRef = useRef<HTMLElement>(null);
 	const translatedMenu = menu.map(o => ({...o, label: t(o.label)}));
-	useEffect(() => {
-		const bkg = new Image();
-		bkg.src = bkgUrl;
-		bkg.onload = () => {
-			if (bkgRef.current) {
-				bkgRef.current.style.opacity = "0.75";
-				bkgRef.current.style.filter = "blur(2px)";
-			}
-		};
-	}, []);
-
+	
 	return <div className="p-features">
-		<Box
-			ref={bkgRef}
-			sx={{ 
-				filter: "blur(20px)",
-				opacity: 0,
-				backgroundImage: `url(${bkgUrl})`
-			}} 
-			className="p-features__background"
-		>
-		</Box>
 		<Box className="p-features__content">
 			<XCenter zIndex={2}>
 				<Typography variant="h1">
