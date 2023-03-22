a87e6e5f011ab28b673cd27c73ffae699858e6d6
ClementSyu <clement.syu.csf@gmail.com>
2023-03-22 18:37:05 +0800

    feat: PNavlink external link
    topic
    - make PNavlink capable of utilizing external link
    
    modified
    - src\components\molecules\pNavLink\index.tsx
    - src\components\molecules\pNavLink\typing.ts
    - src\components\organisms\collapseMenu\index.tsx
    - src\components\organisms\collapseMenu\typing.ts

--- src/components/organisms/collapseMenu/index.tsx
+++ src/components/organisms/collapseMenu/index.tsx
@@ -1,16 +1,15 @@
 import React, { useCallback, useEffect, useState } from "react";
 import Stack from "@mui/material/Stack";
 import { PNavLink } from "src/components/molecules/pNavLink";
-import { useTranslation } from "react-i18next";
 import { CollapseMenuProps } from "./typing";
 import "./style.scss";
 import { collapseMenuStyleCreator } from "./lib";
 import { useAppSelector } from "src/app/hooks";
 import { Box } from "@mui/material";
+
 export const CollapseMenu = (props: CollapseMenuProps) => {
 	const { items } = props;
 	const [_items, setItems] = useState<CollapseMenuProps["items"]>([]);
-	const { i18n } = useTranslation();
 	const [collapse, setCollapse] = useState<boolean>(true);
 	const loadingQueue = useAppSelector(state => state.loading.loadingQueue);
 	
@@ -31,8 +30,6 @@ export const CollapseMenu = (props: CollapseMenuProps) => {
 		}
 	}, [loadingQueue.length]);
 
-	const lng = i18n.language;
-
 	return <nav>
 		<Stack component={"ul"} className={`o-collapseMenu${collapse ? " o-collapseMenu--collapse m-pNavLink__controlledHover ": " "}overflow-hidden`} alignItems={{ xs: "center" }}>
 			{
@@ -45,13 +42,12 @@ export const CollapseMenu = (props: CollapseMenuProps) => {
 						key={i}
 					>
 						<PNavLink
-							url={`/${lng}`+ item.url}
 							delay={600}
 							callback={handleCollapse}
-							// text={t(name as LanguageResourceKeysContent)}
 							text={item.label}
 							activateLoading={item.activateLoading}
 							fontSize="2rem"
+							{...item}
 						/>
 					</Box>;
 				})
