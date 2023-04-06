9ca6de10147a71328e1ca7f3cd570c2281f743b5
ClementSyu <clement.syu.csf@gmail.com>
2023-04-06 15:58:25 +0800

    feat: cross hair
    topic
    - cross hair
    
    created
    - src\components\organisms\stockChart\style.scss
    
      canvas position style
    
    - src\components\organisms\stockChart\lib\crossHair.ts
    
      a class drawing cross hair
    
    modified
    
    - src\components\organisms\stockChart\index.tsx
    
      add canvas element for cross hair

--- src/components/organisms/stockChart/index.tsx
+++ src/components/organisms/stockChart/index.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef } from "react";
 import { Box } from "@mui/material";
 import { weightedData } from "./const";
 import { KLines } from "./lib/kLines";
+import "./style.scss";
+import { CrossHair } from "./lib/crossHair";
 
 function getElementSize(element: HTMLElement) {
 	const width = element.offsetWidth;
@@ -11,24 +13,32 @@ function getElementSize(element: HTMLElement) {
 
 const _StockChart = () => {
 	const kLines = useRef<KLines|null>(null);
+	const crossHair = useRef<CrossHair|null>(null);
 	const chartCanvas = useRef<HTMLCanvasElement>(null);
-
+	const foregroundCanvasRef = useRef<HTMLCanvasElement>(null);
 	useEffect(() => {
-		if (chartCanvas.current && chartCanvas.current.getContext("2d") && chartCanvas.current.parentElement) {
+		if (
+			chartCanvas.current &&
+			foregroundCanvasRef.current &&
+			chartCanvas.current.getContext("2d") && 
+			chartCanvas.current.parentElement
+		) {
+			const bodySize = getElementSize(document.body);
 			const wrapperSize = getElementSize(chartCanvas.current.parentElement);
 			chartCanvas.current.width = wrapperSize.width;
-			chartCanvas.current.height = wrapperSize.height;
+			chartCanvas.current.height = bodySize.height;
+			foregroundCanvasRef.current.width = wrapperSize.width;
+			foregroundCanvasRef.current.height = bodySize.height;
 			kLines.current = new KLines(chartCanvas.current, weightedData);
+			crossHair.current = new CrossHair(foregroundCanvasRef.current, chartCanvas.current);
 		} else {
 			alert("不支援CanvasRenderingContext2D");
 		}
-		return () => {
-			kLines.current = null;
-		};
 	}, []);
 
 	return <Box>
-		<canvas width={800} height={600} ref={chartCanvas}></canvas>
+		<canvas className="o-stockChart__kLine" ref={chartCanvas}></canvas>
+		<canvas className="o-stockChart__foreground" ref={foregroundCanvasRef}></canvas>
 	</Box>; 
 };
 
---
+++ src/components/organisms/stockChart/lib/crossHair.ts
@@ -0,0 +1,42 @@
+import { BasicCanvas } from "./basic";
+
+export class CrossHair extends BasicCanvas {
+	protected canvas: HTMLCanvasElement;
+	protected chartCanvas: HTMLCanvasElement;
+	// ctx: CanvasRenderingContext2D;
+	canvasRect: DOMRect;
+	canvasX: number;
+	canvasY: number;
+
+	constructor(canvas: HTMLCanvasElement, chartCanvas: HTMLCanvasElement) {
+		super(canvas);
+		this.canvas = canvas;
+		this.chartCanvas = chartCanvas;
+		// this.ctx = canvas.getContext("2d")!;
+		this.ctx.strokeStyle = "#000";
+		this.ctx.setLineDash([5, 5]);
+		this.ctx.lineWidth = 1;
+		// 計算畫布在文檔中的位置
+		this.canvasRect = this.canvas.getBoundingClientRect();
+		this.canvasX = this.canvasRect.left;
+		this.canvasY = this.canvasRect.top;
+		this.canvas.addEventListener("mousemove", this.drawCrossHair);
+	}
+
+	drawCrossHair = (event: MouseEvent) => {
+		this.clear();
+		const x = event.clientX - this.canvasX;
+		const y = event.clientY - this.canvasY;
+		// 畫水平線
+		this.ctx.beginPath();
+		this.ctx.moveTo(0, y);
+		this.ctx.lineTo(this.canvas.width, y);
+		this.ctx.stroke();
+
+		// 畫垂直線
+		this.ctx.beginPath();
+		this.ctx.moveTo(x, 0);
+		this.ctx.lineTo(x, this.canvas.height);
+		this.ctx.stroke();
+	};
+}
