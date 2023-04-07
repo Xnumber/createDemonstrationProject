5edbce79d269ce882fec4894deef4474ec758563
ClementSyu <clement.syu.csf@gmail.com>
2023-04-07 15:11:58 +0800

    feat: loading message update
    topic
    - message to notify user that they can cancel the extension of the display time
    
    modified
    - src\app\language\locales\en\home.json
    - src\app\language\locales\zh\home.json
    
      translation
    
    - src\components\organisms\loading\index.tsx
    
      user interface

--- src/components/organisms/loading/index.tsx
+++ src/components/organisms/loading/index.tsx
@@ -4,19 +4,21 @@ import { XCenter } from "templates/xCenter";
 import Cookies from "js-cookie";
 import { LoadingState } from "features/loading/type";
 import { useTranslation } from "react-i18next";
-
+import { useAppSelector } from "src/app/hooks";
 const _LoadingBackdrop = (props: { events: LoadingState["loadingQueue"] }) => {
 	const { events } = props;
 	const xsrfToken = Cookies.get("XSRF-TOKEN");
 	const { t } = useTranslation("home");
+	const displayEventsLonger = useAppSelector(state => state.loading.displayEventsLonger);
+	
 	return <Portal container={document.body}>
 		<Backdrop open={events.length !== 0}>
 			{
 				events.length === 1 && events[0].event === "Complete"  ? <Typography variant="h2" color={"#fff"}>
 					{t("loading-complete")}
 				</Typography>: 
-					<div>
-						<XCenter mb={7}>
+					<Box width={"100%"}>
+						<XCenter mb={3}>
 							<CircularProgress size={60}/>
 						</XCenter>
 						<Box mb={2}>
@@ -30,7 +32,15 @@ const _LoadingBackdrop = (props: { events: LoadingState["loadingQueue"] }) => {
 									</Typography></>: null
 							}
 						</Box>
-						<div>
+						{
+							displayEventsLonger ?
+								<XCenter mb={2}>
+									<Typography p={1} sx={{ background: "var(--md-sys-color-primary)"}} maxWidth={"600px"} variant="body1" color={"var(--md-sys-color-on-primary)"}>
+										{t("common-loading-message")}
+									</Typography>
+								</XCenter>: null
+						}
+						<XCenter>
 							{
 								events.map((o, i) => {
 									return <Box key={i}>
@@ -43,8 +53,8 @@ const _LoadingBackdrop = (props: { events: LoadingState["loadingQueue"] }) => {
 									</Box>;
 								})
 							}
-						</div>
-					</div>
+						</XCenter>
+					</Box>
 			}
 		</Backdrop>
 	</Portal>;
