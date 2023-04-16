4cfe5eb9a4b1979da956dfab834a2b21e0d70012
ClementSyu <clement.syu.csf@gmail.com>
2023-04-16 14:14:05 +0800

    feat: delete unused lines
    topic
    - delete unused lines
    
    modified
    - src\components\organisms\stockChart\index.tsx

--- src/components/organisms/stockChart/index.tsx
+++ src/components/organisms/stockChart/index.tsx
@@ -1,89 +1,23 @@
-import React, { useEffect, useRef, useState, useCallback } from "react";
+import React, { useEffect, useRef } from "react";
 import { Box } from "@mui/material";
-import { stockGraphLibNames, weightedData } from "./const";
+import { weightedData } from "./const";
 import { KLines } from "./lib/kLines";
 import "./style.scss";
 import { CrossHair } from "./lib/crossHair";
 import { StockChartHeader } from "./lib/header";
 import { useAppSelector } from "src/app/hooks";
-import { MovingAerage } from "./lib/movingAverage";
-import { Line } from "./lib/line";
 import { Chart } from "./lib/chart";
-import { StockGraphLibName } from "./lib/type";
-import { MultipleSelect } from "molecules/multipleSelect";
 import { Link } from "react-router-dom";
 
-function getElementSize(element: HTMLElement) {
-	const width = element.offsetWidth;
-	const height = element.offsetHeight;
-	return { width, height };
-}
-
 const _StockChart = () => {
-
 	const chartRef = useRef<Chart|null>(null);
 	const chartContainerRef = useRef<HTMLDivElement>(null);
 	const kLines = useRef<KLines|null>(null);
 	const crossHair = useRef<CrossHair|null>(null);
 	const stockChartHeader = useRef<StockChartHeader|null>(null);
-	const line = useRef<Line|null>(null);
-	const movingAerage = useRef<MovingAerage|null>(null);
-	const chartCanvas = useRef<HTMLCanvasElement>(null);
-	const foregroundCanvasRef = useRef<HTMLCanvasElement>(null);
-	const chartHeaderCanvasRef = useRef<HTMLCanvasElement>(null);
-	const movingAerageCanvasRef = useRef<HTMLCanvasElement>(null);
-	const lineCanvaseRef = useRef<HTMLCanvasElement>(null);
 	const mode = useAppSelector(state => state.theme.mode);
-	const [chosenGraphLibNames, setChosenGraphLibNames] = useState<StockGraphLibName[]>(["Grid", "KLines"]);
-	// const stockChartType = useState<StockChartType>("k-line");
 	const initialRender = useRef(true);
-	useEffect(() => {
-		if (
-			lineCanvaseRef.current &&
-			chartHeaderCanvasRef.current &&
-			movingAerageCanvasRef.current &&
-			chartCanvas.current &&
-			foregroundCanvasRef.current &&
-			chartCanvas.current.getContext("2d") && 
-			chartCanvas.current.parentElement
-		) {
-			const wrapperSize = getElementSize(chartCanvas.current.parentElement);
-			chartCanvas.current.width = wrapperSize.width;
-			chartCanvas.current.height = wrapperSize.height;
-			movingAerageCanvasRef.current.width = wrapperSize.width;
-			movingAerageCanvasRef.current.height = wrapperSize.height;
-			foregroundCanvasRef.current.width = wrapperSize.width;
-			foregroundCanvasRef.current.height = wrapperSize.height;
-			chartHeaderCanvasRef.current.width = wrapperSize.width;
-			chartHeaderCanvasRef.current.height = 100;
-			lineCanvaseRef.current.width = wrapperSize.width;
-			lineCanvaseRef.current.height = wrapperSize.height;
-			kLines.current = new KLines(chartCanvas.current, weightedData, mode);
-			// line.current = new Line(lineCanvaseRef.current, weightedData, foregroundCanvasRef.current, mode);
-			crossHair.current = new CrossHair(foregroundCanvasRef.current, chartCanvas.current, mode);
-			stockChartHeader.current = new StockChartHeader(chartHeaderCanvasRef.current, kLines.current, foregroundCanvasRef.current, mode);
-			// movingAerage.current = new MovingAerage(movingAerageCanvasRef.current, kLines.current, foregroundCanvasRef.current, mode);
-		} else {
-			alert("不支援CanvasRenderingContext2D");
-		}
-
-		return () => {
-			kLines.current?.destroy();
-			crossHair.current?.destroy();
-			stockChartHeader.current?.destroy();
-			movingAerage.current?.destroy();
-			line.current?.destroy();
-		};
-	}, []);
-
-	const handleChosenGraphLibNames = useCallback((e: string[]|string) => {
-		if(typeof e === "string") {
-			setChosenGraphLibNames([e as StockGraphLibName]);
-		} else {
-			setChosenGraphLibNames(e as StockGraphLibName[]);
-		}
-	}, []);
-
+	
 	useEffect(() => {
 		crossHair.current?.setMode(mode);
 		stockChartHeader.current?.setMode(mode);
@@ -99,16 +33,10 @@ const _StockChart = () => {
 				chartContainerRef.current,
 				weightedData,
 				mode,
-				chosenGraphLibNames
+				["Grid", "KLines"]
 			);
 		}
 	}, []);
-	
-	useEffect(() => {
-		if(chartRef.current && !initialRender.current) {
-			chartRef.current.updateGraphLib(chosenGraphLibNames);
-		}
-	}, [chosenGraphLibNames]);
 
 	useEffect(() => {
 		initialRender.current = false;
@@ -119,22 +47,8 @@ const _StockChart = () => {
 
 	return <Box>
 		<Link to="/zh">zh</Link>
-		<MultipleSelect
-			defaultSelected={chosenGraphLibNames}
-			options={stockGraphLibNames.map(sgl => ({ value: sgl, label: sgl}))}
-			callback={handleChosenGraphLibNames}
-		/>
 		<Box position={"relative"} height={"360px"} ref={chartContainerRef}>
 
-		</Box>
-		<div>
-			<canvas className="o-stockChart__header" ref={chartHeaderCanvasRef}></canvas>
-		</div>
-		<Box height={"360px"}>
-			<canvas className="o-stockChart__kLine" ref={chartCanvas}></canvas>
-			<canvas className="o-stockChart__movingAverage" ref={movingAerageCanvasRef}></canvas>
-			<canvas className="o-stockChart__line" ref={lineCanvaseRef}></canvas>
-			<canvas className="o-stockChart__foreground" ref={foregroundCanvasRef}></canvas>
 		</Box>
 	</Box>;
 };
